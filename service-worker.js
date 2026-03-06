f355a8375ef50e94e7b7623e85fe90b278e69ce3
jjproo <86826376+jjproo@users.noreply.github.com>
2026-03-06 17:47:42 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+
+const CACHE_NAME='inventory-mobile-offline-v1';
+const urlsToCache=['./','./index.html','./manifest.json','./css/style.css','./js/app.js','./sample-data.json','./icons/icon-192.png','./icons/icon-512.png','https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):Promise.resolve())))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
